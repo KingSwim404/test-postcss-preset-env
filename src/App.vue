be2705f295cae72c965f8b52e8e4cdae217bdcca
KingSwim <15271932314@163.com>
2019-08-07 09:37:25 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+
+        <div id="test">
+
+        </div>
+
+    </div>
+</template>
+
+<style>
+
+    html, body {
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        background-color: var(--main-color);
+        width: 100%;
+        height: 100%;
+    }
+
+    #test {
+        width: 100px;
+        height: 100px;
+        background-color: yellow;
+    }
+
+    @media (--mobile-media) {
+        #test {
+            height: 200px;
+            background-color: var(--mobile-color);
+        }
+
+    }
+
+</style>
